274bbbfb672eb87f108e4be43aeefe94612e1177
Khodzha Abulkhair <khodzha_au@mai.ru>
2018-05-16 17:39:33 +0600

    indexpage

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -39,6 +39,30 @@
       </div>
     </div>
   </div>
+<!-- end of first-row -->
+  <div class="second-row">
+      <h2>Каталог строительных материалов</h2>
+      <div>
+        <a href="#spec" class="catalogue"><span>Стройматериалы</span></a>
+        <a href="#spec" class="catalogue"><span>Отделочные материалы</span></a>
+        <a href="#spec" class="catalogue"><span>Сантехника, отопление и водоснабжение</span></a>
+        <a href="#spec" class="catalogue"><span>Электрика и освещение</span></a>
+        <a href="#spec" class="catalogue"><span>Геоматериалы</span></a>
+        <a href="#spec" class="catalogue"><span>Инструмент и оборудование</span></a>
+        <a href="#spec" class="catalogue"><span>Дача и сад</span></a>
+        <a href="#spec" class="catalogue"><span>Акции</span></a>
+      </div>
+  </div>
+<!-- end of second-row -->
+  <div class="third-row">
+      <h2>Мы в соц.сетях</h2>
+      <div>
+        <a href="#sc" class="social"><span></span>jana-aigerim</a>
+        <a href="#sc" class="social"><span></span>jana-aigerim</a>
+        <a href="#sc" class="social"><span></span>jana-aigerim</a>
+        <a href="#sc" class="social"><span></span>jana-aigerim</a>
+      </div>
+  </div>
 </div>
 
 @endsection
